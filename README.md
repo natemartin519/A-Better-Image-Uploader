fcc475fde46c6f336f68e6be7a3c0d751dc6b1c0
natemartin519 <natemartin519@gmail.com>
2014-03-12 21:30:47 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Image Uploader [![Build Status](https://secure.travis-ci.org/natemartin519/image-uploader.png?branch=master)](http://travis-ci.org/natemartin519/image-uploader)
+
+Yo it's Node uploading images!
+
+## Getting Started
+Install the module with: `npm install image-uploader`
+
+```javascript
+var image-uploader = require('image-uploader');
+image-uploader.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2014 Nathan Martin. Licensed under the MIT license.
---
+++ test/image-uploader_test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var imageUploader = require('../lib/image-uploader.js');
+
+/*
+  ======== A Handy Little Nodeunit Reference ========
+  https://github.com/caolan/nodeunit
+
+  Test methods:
+    test.expect(numAssertions)
+    test.done()
+  Test assertions:
+    test.ok(value, [message])
+    test.equal(actual, expected, [message])
+    test.notEqual(actual, expected, [message])
+    test.deepEqual(actual, expected, [message])
+    test.notDeepEqual(actual, expected, [message])
+    test.strictEqual(actual, expected, [message])
+    test.notStrictEqual(actual, expected, [message])
+    test.throws(block, [error], [message])
+    test.doesNotThrow(block, [error], [message])
+    test.ifError(value)
+*/
+
+exports.imageUploader = {
+  setUp: function(done) {
+    // setup here
+    done();
+  },
+  'no args': function(test) {
+    test.expect(1);
+    // tests here
+    test.equal(imageUploader.awesome(), 'awesome', 'should be awesome.');
+    test.done();
+  }
+};
