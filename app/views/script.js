6ae34ceaa935921fc2311d30ca26eed22f0d9c74
natemartin519 <natemartin519@gmail.com>
2014-03-20 15:06:54 -0400

    Added AJAX for image list. Added AJAX for upload. Some server valadation for POST upload

---
+++ app/views/script.js
@@ -0,0 +1,37 @@
+$(function() {
+    $(".js-hidden").css("visibility", "visible");
+
+    function updateImageList() {
+        $.ajax({
+            url: '/images'
+        }).done(function(response){
+            var template = $("#imageListTemplate").html();
+            var imageList = '';
+
+            $.each(response.images, function(key, value) {
+                imageList += template.replace(/{{name}}/ig, value)
+            });
+
+            $("#imageList").html(imageList);
+        });
+    }updateImageList();
+
+    $("form#imageForm").submit(function(event) {
+        event.preventDefault();
+
+        var $form = $(this)[0];
+        var formData = new FormData($form);
+        formData.append("noRedirect", true);
+
+        $.ajax({
+            url: '/upload',
+            type: 'POST',
+            contentType: false,
+            processData: false,
+            data: formData
+        }).done(function(response){
+            updateImageList();
+            $form.reset();
+        });
+    });
+});
